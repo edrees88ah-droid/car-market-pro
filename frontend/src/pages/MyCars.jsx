b6b4030d0ef1efe73a5a7592be1018780fff0b20
edrees88ah-droid <edrees88ah@gmail.com>
2026-02-25 10:23:52 +0200

    Update MyCars.jsx

--- frontend/src/pages/MyCars.jsx
+++ frontend/src/pages/MyCars.jsx
@@ -9,7 +9,7 @@ const apiBase = import.meta.env.VITE_API_URL || "http://localhost:5000";
 const MyCars = () => {
     const [cars, setCars] = useState([]);
     const [loading, setLoading] = useState(true);
-    const apiBase = "${apiBase}";
+    const apiBase = '${apiBase}';
 
     // 1. جلب البيانات من السيرفر
     const loadData = async () => {
@@ -188,3 +188,4 @@ const MyCars = () => {
 
 
 export default MyCars;
+
