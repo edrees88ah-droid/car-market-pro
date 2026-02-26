0c2b4e399242218f9ba76f39b6bf5a00d948a9c9
edrees88ah-droid <edrees88ah@gmail.com>
2026-02-26 11:42:25 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-//import dotenv from 'dotenv';
-//dotenv.config();
+import dotenv from 'dotenv';
+dotenv.config();
 export default pool;
 const pool = new Pool({
   // الرابط سيأتي من إعدادات Vercel
@@ -19,6 +19,7 @@ pool.on('error', (err) => {
 });
 
 export default pool;
+
 
 
 
