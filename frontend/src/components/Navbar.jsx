47b473a0ba6d692908e377c2eb7e2eb93d042e58
edrees88ah-droid <edrees88ah@gmail.com>
2026-02-27 14:39:45 +0200

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     
     const token = localStorage.getItem('token');
     const user = JSON.parse(localStorage.getItem('user'));
-    const apiBase = "http://localhost:5000";
+   // const apiBase = "http://localhost:5000";
 
     // 1. جلب عدد الإشعارات غير المقروءة
     const fetchUnreadCount = async () => {
@@ -137,5 +137,6 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
 
