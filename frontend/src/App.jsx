6fad19d4eb1acf1842ea19ef13d674347a010cdd
edrees88ah-droid <edrees88ah@gmail.com>
2026-03-02 10:32:10 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,9 +24,9 @@ function App() {
   const [loading, setLoading] = useState(true);
   
   // جلب البيانات من localStorage بأمان
-  //const token = localStorage.getItem('token');
- // const storedUser = localStorage.getItem('user');
- // const user = storedUser ? JSON.parse(storedUser) : null;
+  const token = localStorage.getItem('token');
+  const storedUser = localStorage.getItem('user');
+  const user = storedUser ? JSON.parse(storedUser) : null;
 
   // محاولة قراءة المستخدم بأمان لمنع الانهيار ✅
   const user = (() => {
